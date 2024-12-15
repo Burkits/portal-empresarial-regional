d6bc12b5f648c00dea72748e128ffaf74de4953f
Burkits <104347202+Burkits@users.noreply.github.com>
2024-12-14 23:26:16 -0600

    interaction

---
+++ story_content/triggers.js
@@ -0,0 +1,21 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ewymBIeRlp":
+        Script1();
+        break;
+      case "6Q9Vfv0No3e":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
